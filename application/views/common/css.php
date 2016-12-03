b0a025983a9a2426a48361263aeff8b5d8441b6b
katefrz <kate_freeze@yahoo.com>
2016-12-03 20:16:52 +0700

    PUSHED

---
+++ application/views/common/css.php
@@ -0,0 +1,23 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Creative - Start Bootstrap Theme</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic" rel="stylesheet" type="text/css">
+
+    <!-- Plugin CSS -->
+    <link href="<?php echo base_url('assets/css/magnific-popup.css'); ?>" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="<?php echo base_url('assets/css/creative.min.css'); ?>" rel="stylesheet">
+</head>
---
+++ application/views/common/js.php
@@ -0,0 +1,13 @@
+	<!-- jQuery -->
+    <script src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="<?php echo base_url('assets/js/scrollreveal.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/jquery.magnific-popup.min.js'); ?>"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="<?php echo base_url('assets/js/creative.min.js'); ?>"></script>
